eb3b378f630be3b14762d2cc87bd84f6d7112f4c
241111097@student.unu-jogja.ic.id <email>
2025-06-26 12:31:13 +0700

    uhuy

---
+++ sipaCantik.py
@@ -0,0 +1,6 @@
+sipa = "Cipa adalah mahasiswa tercantik di unuyo"
+
+if "tercantik" in sipa:
+    print("yeeeeyyyyyyyyy cipa cantik banget aduh kiw kiw kiw semlehot")
+else:
+    print("aduhay semlehoy")
